e4562f79c16dd42fc554ffb3ba2ce9c2b33b56ed
Vassardog77 <benmoxon256@gmail.com>
2023-05-26 19:03:46 -0700

    instagram posting error fix

--- client/src/components/Feed/Form.js
+++ client/src/components/Feed/Form.js
@@ -6,10 +6,9 @@ import axios from 'axios';
 import InstagramLogin from "../MediaLogin/InstagramLogin"
 import FacebookLogin from "../MediaLogin/FacebookLogin"
 import { base_url } from '../../api';
-function Form(props) {
-    let [Content, setContent] = useState()
-    const [instagram_login, setInstagramLogin] = useState(localStorage.getItem('instagram_login'));
 
+function Form(props) {
+    const [instagram_login, setInstagramLogin] = useState(localStorage.getItem('instagram_login'));
     const current_user = JSON.parse(localStorage.getItem('user'))
 
     const [postData, setPostData] = useState({ creator: current_user.email, message: '', tags: '', selectedFile: '' });
@@ -33,6 +32,7 @@ function Form(props) {
             alert("Post Created!")
             setPostData({ creator: current_user.email, message: '', tags: '', selectedFile: '' });
         } else if (MediaSelector === "Instagram") { //if its set to instagram, post to instagram
+            console.log(postData)
             axios.post(base_url+"/post/ig1", postData)
             .then((response) => {
                 console.log(JSON.stringify(response.data));
@@ -41,19 +41,16 @@ function Form(props) {
                 setPostData({ creator: current_user.email, message: '', tags: '', selectedFile: '' });
               })
               .catch((error) => {
-                alert("Error Creating Post. Refresh and try again.")
+                alert("Error Creating Post. \nPlease make sure that you are logged in and try again")
                 console.log(error);
               });
-              
         }
     }
 
     useEffect(() => {
-        //console.log(CreationId)
         if(CreationId === "") {
             return
         }
-        //console.log("postsing "+CreationId)
         axios.post(base_url+"/post/ig2", {
             "creation_id":CreationId,
             "user": current_user.email
@@ -64,33 +61,11 @@ function Form(props) {
         setPostData({ creator: current_user.email, message: '', tags: '', selectedFile: '' });
     }
 
-
     useEffect(() => {
-         if ((instagram_login === 'false' || instagram_login === null) && MediaSelector === "Instagram") {
-            setContent(
-              <div className='login_message'>
-                <div>Please log in with instagram to continue</div>
-                <div className='Loginbar'>
-                    <div><FacebookLogin></FacebookLogin></div>
-                    <div><InstagramLogin></InstagramLogin></div>
-                </div>
-              </div>
-            )
-          } else {
-            setContent(
-                <div>
-                        <form onSubmit={handleSubmit}>
-                            <div>
-                                <FileBase type='file' multiple={false} onDone={({base64}) =>setPostData({ ...postData, selectedFile: base64})}></FileBase>
-                            </div>
-                            <textarea  placeholder='Message' value={postData.message}onChange={(e) => setPostData({ ...postData, message: e.target.value })}></textarea>
-                            <textarea  placeholder='Tags' value={postData.tags}onChange={(e) => setPostData({ ...postData, tags: e.target.value })}></textarea>
-                            <button>Submit</button>
-                            <button type="button" onClick={clear}>Clear</button>
-                        </form>
-                </div>)
-          }
-      }, [setContent,instagram_login,MediaSelector])
+        if ((instagram_login === 'false' || instagram_login === null) && MediaSelector === "Instagram") {
+            console.log("not logged in with instagram")
+        }
+    }, [instagram_login,MediaSelector])
 
     return (
         <div>
@@ -99,7 +74,27 @@ function Form(props) {
                 <button onClick={change_mediaselector_instagram}>Instagram</button>
                 <div>Posting to: {MediaSelector}</div>
             </div>
-            {Content}
+            {(instagram_login === 'false' || instagram_login === null) && MediaSelector === "Instagram" ? (
+                <div className='login_message'>
+                    <div>Please log in with Instagram to continue</div>
+                    <div className='Loginbar'>
+                        <div><FacebookLogin></FacebookLogin></div>
+                        <div><InstagramLogin></InstagramLogin></div>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <FileBase type='file' multiple={false} onDone={({base64}) =>setPostData({ ...postData, selectedFile: base64})}></FileBase>
+                        </div>
+                        <textarea  placeholder='Message' value={postData.message}onChange={(e) => setPostData({ ...postData, message: e.target.value })}></textarea>
+                        <textarea  placeholder='Tags' value={postData.tags}onChange={(e) => setPostData({ ...postData, tags: e.target.value })}></textarea>
+                        <button>Submit</button>
+                        <button type="button" onClick={clear}>Clear</button>
+                    </form>
+                </div>
+            )}
         </div>
     );
 }
