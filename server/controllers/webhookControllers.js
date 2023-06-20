fde4caa456b9160b4552b6f56d88f489ddb17634
Vassardog77 <benmoxon256@gmail.com>
2023-06-19 21:19:07 -0700

    webhook update 3

--- server/controllers/webhookControllers.js
+++ server/controllers/webhookControllers.js
@@ -1,19 +1,22 @@
 import axios from "axios"
 
-export const postWebhooks = async (req, res) => { // webhook endpoints
+export const postWebhooks = (req, res) => {
     console.log(req)
-    // parse the query parameters from the request
-    let mode = req.query["hub.mode"];
-    let token = req.query["hub.verify_token"];
-    let challenge = req.query["hub.challenge"];
-
-    // Check the mode and token sent is correct
-    if (mode === "subscribe" && token === "teststring") { // replace "teststring" with your verify token
-        console.log("WEBHOOK_VERIFIED");
+    // Parse the query parameters
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+  
+    // Check if a token and mode were sent
+    if (mode && token) {
+      // Check the mode and token sent are correct
+      if (mode === 'subscribe' && token === "teststring") {
         // Respond with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
         res.status(200).send(challenge);
-    } else {
-        // Respond with '403 Forbidden' if verify tokens do not match
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
         res.sendStatus(403);
+      }
     }
-};
+  };
